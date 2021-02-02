32aa6cb06d6cd3d4550e88538a6ecdecbf707517
Ronald <ronald.eijsden@gmail.com>
2021-02-02 23:16:53 +0100

    elevator v2 solution removed

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
 function elevatorDirection(buttons_pressed=null, your_choice, current_floor) {
+    //write
 
-    let value=null;
-
-    if(your_choice > current_floor)
-        value="Up";
-    else if (your_choice < current_floor)
-        value="Down";
-
-    if(floorPriority(buttons_pressed, current_floor) > your_choice)
-        value="Up";
-    else if(floorPriority(buttons_pressed, current_floor) < your_choice)
-        value="Down";
-
-    return value;
 }
 
 function floorPriority(buttons_pressed, current_floor){
+    //write
 
-    let output;
-
-    for(let i=0; i<buttons_pressed.length; i++){
-        if(buttons_pressed[i]!==current_floor){
-            output = buttons_pressed[i];
-            break;
-        }
-    }
-
-    return output;
 }
 
 function sameFloor(your_choice, current_floor){
-    return your_choice === current_floor;
+    //write
+
 }
 
 module.exports = {
